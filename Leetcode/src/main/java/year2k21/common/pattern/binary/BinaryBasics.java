263c93aeeaf2e74f4538008265686a979003ae61
PintuB <pintupnt542@gmail.com>
2022-08-29 23:22:24 +0530

    Update BinaryBasics.java

--- Leetcode/src/main/java/year2k21/common/pattern/binary/BinaryBasics.java
+++ Leetcode/src/main/java/year2k21/common/pattern/binary/BinaryBasics.java
@@ -16,7 +16,7 @@ public class BinaryBasics {
         int bit = 3;//Question asks for 4th bit. Thus shift "1" only 3 times, so 1 becomes 1000(Notice,1 in 4th position) in binary
         System.out.println(a | (1 << bit));
 
-        //Set 4th bit(from right-side) of "a" as 0 "Or" CLear 4th bit of "a"
+        //Claer 4th bit(from right-side) of "a" as 0 "Or" Clear 4th bit of "a"
         //Intuition : a | 0111
         bit = 3;
         System.out.println(a | ~(1 << bit));
@@ -34,12 +34,12 @@ public class BinaryBasics {
         a = 5;
         System.out.println(a & 1);
 
-        //Clear last 1-Bit from "a", Example : a = 10011000, output = 10010000
-        //Hard to Figure out, But HINT : To retain a bit you can do & operation on same bit or do & operation with 1. So, solution can be "either" 10011000 & 11110111 "or" 10011000 & 10010111 (Notice, 10010111 = 10011000 - 1)
+        //Clear last 1-Bit from "a" & retain other bits, Example : a = 10011000, output = 10010000
+        //Hard to Figure out, But HINT : To retain a bit you can do "&" operation on same bit or do & operation with 1. So, solution can be "either" 10011000 & 11110111 "or" 10011000 & 10010111 (Notice, 10010111 = 10011000 - 1)
         //Try to remember
         System.out.println(a & (a-1));
 
-        //Retain last 1-Bit and mark other bits to 0
+        //Retain last 1-Bit and mark other bits to 0, ie. Ulta of above
         //Remember
         System.out.println(a & -a);
     }
