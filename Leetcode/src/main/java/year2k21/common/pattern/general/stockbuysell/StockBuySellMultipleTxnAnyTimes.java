a9b9495180e926e1e176efb5b1837d2a5fcbefb3
PintuB <pintupnt542@gmail.com>
2024-04-14 22:29:15 +0530

    Update StockBuySellMultipleTxnAnyTimes.java

--- Leetcode/src/main/java/year2k21/common/pattern/general/stockbuysell/StockBuySellMultipleTxnAnyTimes.java
+++ Leetcode/src/main/java/year2k21/common/pattern/general/stockbuysell/StockBuySellMultipleTxnAnyTimes.java
@@ -1,4 +1,7 @@
 package year2k21.common.pattern.general.stockbuysell;
 
+/**
+* Another similar Question Buy-Sell for max Profit at most k times https://www.geeksforgeeks.org/maximum-profit-by-buying-and-selling-a-share-at-most-k-times/  
+  **/
 public class StockBuySellMultipleTxnAnyTimes {
 }
