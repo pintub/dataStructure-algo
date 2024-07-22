0ac70c42344a542fc2ba205170d5babd9af88b85
PintuB <pintupnt542@gmail.com>
2024-07-22 15:33:20 +0530

    Update SearchInRotatedSortedArray33.java

--- Leetcode/src/main/java/year2k21/common/pattern/sorting/SearchInRotatedSortedArray33.java
+++ Leetcode/src/main/java/year2k21/common/pattern/sorting/SearchInRotatedSortedArray33.java
@@ -14,7 +14,44 @@ public class SearchInRotatedSortedArray33 {
         return search(nums, target, 0, nums.length - 1);
     }
 
-    private int search(int[] nums, int target, int loIdx, int hiIdx) {
+    /**
+    * Iteration
+    */
+    public int search(int[] nums, int target) {
+        int lo = 0;
+        int hi = nums.length - 1;
+
+        while(lo <= hi) {//TODO check
+            int mid = (lo + hi) / 2;
+            if(nums[mid] == target) {
+                return mid;
+            }
+            if(lo == hi) {
+                return -1;
+            }
+            if(lo <= mid - 1 && nums[lo] <= nums[mid - 1]) {//Left sorted
+                if(nums[lo] <= target && target <=  nums[mid - 1]) {//Target is at left, So search at left
+                    hi = mid - 1;
+                } else {//Search at Right
+                    lo = mid + 1;
+                }
+            } else if(mid + 1 <= hi){//Right Sorted
+                if(nums[mid + 1] <= target && target <=  nums[hi]) {//Target at Right
+                    lo = mid + 1;
+                } else {//Search at Left
+                    hi = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    
+    /**
+    * Recursion
+    */
+    private int search_Recursion(int[] nums, int target, int loIdx, int hiIdx) {
         if(hiIdx < loIdx) {
             System.out.println("How you found me ???");
             return Integer.MIN_VALUE;
