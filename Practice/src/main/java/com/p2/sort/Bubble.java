9f1f8135893ff37cdcfe7f319e3ff9c1fc5581eb
PintuB <pintupnt542@gmail.com>
2024-08-27 10:26:15 +0530

    Update Bubble.java

--- Practice/src/main/java/com/p2/sort/Bubble.java
+++ Practice/src/main/java/com/p2/sort/Bubble.java
@@ -3,7 +3,7 @@ package com.p2.sort;
 import java.util.Arrays;
 
 /**
- * For each outer-loop "i", Current & Next element comparison AND swapped
+ * For each outer-loop "i", Current & Next element comparison AND swapped (from start to end of Array)
  * In each iteration of outer loop, max value goes to right side
  * Optimization: No no swapping in any iteration, then already Sorted
  */
@@ -17,7 +17,7 @@ public class Bubble {
         for(int i=0; i<input.length - 1; i++){
             boolean swapped = false;
 
-            for(int j=0; j < input.length -i -1 ; j++){//Right most part where max are positioned needs not be compared
+            for(int j=0; j < input.length -i -1 ; j++){//From start to end for each "i"
                 //Compare input[j] and input[j+1] and swap
                 if(input[j] > input[j+1]) {
                     swap(input, j, j + 1);
